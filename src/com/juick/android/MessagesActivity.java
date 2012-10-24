c1ba5cec9a3bce59730d039b13678a1cae81bd3b
Sanny Sanoff <san@sysdate.com>
2012-10-24 05:29:01 +0300

    new global/local search enabled

--- src/com/juick/android/MessagesActivity.java
+++ src/com/juick/android/MessagesActivity.java
@@ -40,45 +40,35 @@ public class MessagesActivity extends FragmentActivity {
     Object restoreData;
     MessagesSource messagesSource;
 
+    public boolean shouldDelayLaunch() {
+        return false;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ExceptionReporter.register(this);
         Utils.updateTheme(this);
         super.onCreate(savedInstanceState);
-
         Intent i = getIntent();
-        messagesSource = (MessagesSource)i.getSerializableExtra("messagesSource");
-        if (messagesSource == null) {
-            messagesSource = new JuickCompatibleURLMessagesSource(this);
+        restoreData = getLastCustomNonConfigurationInstance();
+        if (!shouldDelayLaunch() || restoreData != null) {
+            if (messagesSource == null) {
+                messagesSource = (MessagesSource)i.getSerializableExtra("messagesSource");
+                if (messagesSource == null) {
+                    messagesSource = new JuickCompatibleURLMessagesSource(this);
+                }
+            }
+            initWithMessagesSource();
         }
+    }
+
+    protected void initWithMessagesSource() {
         setTitle(messagesSource.getTitle());
-
-//        if (uid > 0 && uname != null) {
-//            setTitle("@" + uname);
-//        } else if (search != null) {
-//            setTitle(getResources().getString(R.string.Search) + ": " + search);
-//        } else if (tag != null) {
-//            String title = getResources().getString(R.string.Tag) + ": " + tag;
-//            if (uid == -1) {
-//                title += " (" + getResources().getString(R.string.Your_messages) + ")";
-//            }
-//            setTitle(title);
-//        } else if (place_id > 0) {
-//            setTitle("Location");
-//        } else if (myBlog) {
-//            setTitle(getResources().getString(R.string.My_blog));
-//        } else {
-//            setTitle(getResources().getString(R.string.All_messages));
-//        }
-
         setContentView(R.layout.messages);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        restoreData = getLastCustomNonConfigurationInstance();
         mf = new MessagesFragment(restoreData, this);
         Bundle args = new Bundle();
-
         args.putSerializable("messagesSource", messagesSource);
-
         mf.setArguments(args);
         ft.replace(R.id.messagesfragment, mf);
         ft.commit();
---
+++ src/com/juick/android/SearchActivity.java
@@ -0,0 +1,96 @@
+package com.juick.android;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import com.juick.android.datasource.JuickCompatibleURLMessagesSource;
+import com.juickadvanced.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: san
+ * Date: 10/24/12
+ * Time: 4:57 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SearchActivity extends MessagesActivity {
+
+    @Override
+    public boolean shouldDelayLaunch() {
+        return true;
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handleIntent(getIntent());
+    }
+
+    public void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    public void onListItemClick(ListView l,
+                                View v, int position, long id) {
+        // call detail activity for clicked entry
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doSearch(query);
+        }
+    }
+
+    ArrayList<String> results = new ArrayList<String>();
+
+    class Adapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return results.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return results.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View item, ViewGroup parent) {
+            if (item == null)
+                item = getLayoutInflater().inflate(R.layout.listitem_juickmessage, null);
+            TextView text = (TextView) item.findViewById(R.id.text);
+            text.setText(results.get(position));
+            return item;
+        }
+    }
+
+    private void doSearch(String query) {
+        messagesSource = new JuickCompatibleURLMessagesSource(getString(R.string.Search) + ": " + query, this).putArg("search", Uri.encode(query));
+        initWithMessagesSource();
+    }
+
+    @Override
+    public boolean onSearchRequested() {
+        return super.onSearchRequested();
+    }
+
+    @Override
+    public void startSearch(String initialQuery, boolean selectInitialQuery, Bundle appSearchData, boolean globalSearch) {
+        super.startSearch(initialQuery, selectInitialQuery, appSearchData, globalSearch);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+}
